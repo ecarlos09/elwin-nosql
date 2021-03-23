16e1aedddc1f027810850bdbbfba903e473f1f08
Elwin <elwin.carlos09@gmail.com>
2021-03-23 18:12:55 +0000

    add functions for retrieving celebs with children and counting them too

--- data/helpers.js
+++ data/helpers.js
@@ -1,15 +1,26 @@
 db = connect("localhost:27017/celebs");
 
+function getAndPrettifyCelebs() {
+    return db.celebs.find().pretty();
+}
+
 function getCelebNamesOnly() {
-    let celebItems = db.celebs.find().toArray();
-    let celebNames = celebItems.map(celeb => {
+    let celebsArray = db.celebs.find().toArray();
+    let celebNames = celebsArray.map(celeb => {
         return celeb.name;
     })
     return celebNames;
 }
 
-function getCelebsByChildren() {
+function getCelebsWithChildren() {
+    let celebsWithChildren = db.celebs.find({
+        children: {$ne: 0} 
+    })
+    return celebsWithChildren.pretty();
+}
 
+function countCelebsWithChildren() {
+    return getCelebsWithChildren().toArray().length;
 }
 
 function getCelebsByAwardsWon() {
